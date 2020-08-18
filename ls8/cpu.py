c435294dfde005e3b3dde8387d48d6adf84945f1
Alex Martinez <alexmartinez0071234@gmail.com>
2020-08-18 17:07:43 -0400

    mult done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -18,33 +18,45 @@ class CPU:
         self.ram[address] = value
         # self.ram[address]
 
-    def load(self):
+    file = sys.argv[1]
+    def load(self,file):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        try:
+            # print(sys.argv[1])
+            with open(file) as f:
+                for line in f:
+                    line  = line.split("#")[0].strip()
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    if line == '':
+                        continue
 
+                    self.ram[address] = int(line,2)
+                    address +=1
 
+        except:
+            print("can not find it!")
+            
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        if op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -85,3 +97,8 @@ class CPU:
                 register_address = self.ram_read(self.pc+1)
                 print(self.reg[register_address])
                 self.pc += 2
+            elif IR == 0b10100010:
+                regA = self.ram_read(self.pc+1)
+                regB = self.ram_read(self.pc+2)
+                self.alu("MUL",regA,regB)
+                self.pc +=3
